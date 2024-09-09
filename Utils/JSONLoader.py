684663c9468f9ca3c771a4f75eea3e75543e6cb2
Nora <gdv.nora@gmail.com>
2024-09-09 15:41:55 -0400

    comments

--- Utils/JSONLoader.py
+++ Utils/JSONLoader.py
@@ -15,12 +15,17 @@ class JSONLoader:
     @staticmethod
     def json_to_ramen_list( json_filename) -> list[Ramen]:
         ramen_list = []
+        #Opens JSON file
         with open(json_filename) as file_data:
             json_data = file_data.read()
+        #Reads JSON file into dictionary of values
         data = json.loads(json_data)
+        #For each ramen object inside the JSON dictionary -> Mapping happens here
         for ramen_string in data:
             ramen_list.append( JSONLoader.json_to_ramen(ramen_string) )
         return ramen_list
 
     def insert_ramen(self, ramen: Ramen):
         self.DatabaseContext.insert(ramen)
+
+JSONLoader.json_to_ramen_list("../files/json_test.txt")
