5dcb7067e8d75c50125d3098ba207f928c02b8e5
SaucyB03 <105938116+SaucyB03@users.noreply.github.com>
2024-09-11 09:36:51 -0400

    Re-commit DbInterface
    Was trying to avoid committing the .json file key and accidentally didn't commit my updated DbInterface

--- Database/DbInterface.py
+++ Database/DbInterface.py
@@ -1,5 +1,16 @@
-from Entities import Ramen
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from Entities.Ramen import Ramen
+
 class DbInterface:
+    def __init__(self):
+        # Use a service account.
+        cred = credentials.Certificate('./cs3050-warmup-project-17f83-firebase-adminsdk-1gomb-858737ebab.json')
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+
     def insert(self, ramen_object: Ramen) -> None:
         pass
 
