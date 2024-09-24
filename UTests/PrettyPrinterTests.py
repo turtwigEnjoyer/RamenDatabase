232a6b59b6a960d3554a7d683423c9daec0e5bb6
Nora <gdv.nora@gmail.com>
2024-09-24 17:43:26 -0400

    Updated naming on pretty printer tests

--- UTests/PrettyPrinterTests.py
+++ UTests/PrettyPrinterTests.py
@@ -8,20 +8,19 @@ from Firebase import PrettyPrinter
 class PrettyPrinterTests(unittest.TestCase):
     def setUp(self):
         self.testData = [
-            Ramen.Ramen(_id=1,
-                        brand="Yatekomo",
-                        variety="a",
-                        style="Chicken",
-                        stars=5,
-                        country="Spain",
-                        top_ten="1"),
-            Ramen.Ramen(_id=2,
-                        brand="Yatekomo",
-                        variety="b",
-                        style="Beef And Egg",
-                        stars=4,
-                        country="Spain",
-                        top_ten="2")
+            Ramen.Ramen(Id=1,
+                        Brand="Yatekomo",
+                        Variety="a",
+                        Style="Chicken",
+                        Stars=5,
+                        Country="Spain",
+                        TopTen=True),
+            Ramen.Ramen(Id=2,
+                        Brand="Yatekomo",
+                        Variety="b",
+                        Style="Beef And Egg",
+                        Stars=4,
+                        Country="Spain")
         ]
     def testPrinter(self):
         self.setUp()
