5289a2118d00688b60967be7ca3e6cd9d06fbe12
Nora <gdv.nora@gmail.com>
2024-09-24 16:27:10 -0400

    deleted all commas

--- Main/DbInsertionEngine.py
+++ Main/DbInsertionEngine.py
@@ -7,19 +7,19 @@ PATH_TO_DATASET = "./files/ramen_ratings_short.json"
 #code to populate the database and run a simple query
 
 #Reads a json file and transforms into list
-def json_to_ramen_list( json_filename) -> list[Ramen]:
-        ramen_list = []
-        with open(json_filename) as file_data:
-            json_data = file_data.read()
-        data = json.loads(json_data)
-        for ramen_string in data:
-            ramen_list.append( Ramen(**ramen_string))
-        return ramen_list
+def JsonToRamenList(jsonFilename) -> list[Ramen]:
+        ramenList = []
+        with open(jsonFilename) as fileData:
+            jsonData = fileData.read()
+        data = json.loads(jsonData)
+        for ramenString in data:
+            ramenList.append( Ramen(**ramenString))
+        return ramenList
 
 db = DbInterface()
 
-r_List = json_to_ramen_list(PATH_TO_DATASET)
+ramenList = JsonToRamenList(PATH_TO_DATASET)
 db.ClearDatabase()
 
-for ram in r_List:
+for ram in ramenList:
      db.insert(ram)
